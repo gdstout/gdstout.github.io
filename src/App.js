979deef6c48e89392f4c96034a4f94691645cd90
gdstout <gdstout27@gmail.com>
2021-02-03 18:04:58 -0600

    under construction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import hammer from './images/hammer.gif';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Website under construction</h1>
+      <img src={hammer} alt="under_construction" height="300px"/>
+      <h3>Thank you for your patience!</h3>
     </div>
   );
 }
